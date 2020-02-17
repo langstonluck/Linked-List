55d452c35d663a4c1a92f87b9c5694a2293767b5
langstonluck <langstonluck@langstons-mbp.wireless-1x.unc.edu>
2020-02-17 16:51:56 -0500

    test

--- src/a4/LinkedList.java
+++ src/a4/LinkedList.java
@@ -1,7 +1,32 @@
 package a4;
 
 public class LinkedList<T> {
-
-	// Your code goes here.
+		// Your code goes here.
+	
+	@SuppressWarnings("unused")
+	private Node<T> _head;
+	
+	public int size() {
+		int idx = 0;
+		Node<T> current = _head;
+		if (current == null) {
+			return 0;
+		}
+		while (current.getNext()!= null) {
+			idx++;
+			current = current.getNext();
+		}
+		return idx;
+	}
+	
+	public boolean isEmpty() {
+		if (size() == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	
 	
 }
